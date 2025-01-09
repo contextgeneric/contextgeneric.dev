06c77ddc6b4c961abc68030ebf184501cf30b65b
Soares Chen <soares.chen@maybevoid.com>
2025-01-09 10:36:57 +0000

    Add example code comment

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,43 +1,52 @@
 use cgp::prelude::*;
 
+// Derive CGP provider traits and blanket implementations
 #[cgp_component {
-    name: GreeterComponent,
-    provider: Greeter,
+    name: GreeterComponent, // Name of the CGP component
+    provider: Greeter, // Name of the provider trait
 }]
-pub trait CanGreet {
+pub trait CanGreet // Name of the consumer trait
+{
     fn greet(&self);
 }
 
-#[cgp_auto_getter]
+// A getter trait representing a dependency for `name` value
+#[cgp_auto_getter] // Derive blanket implementation
 pub trait HasName {
     fn name(&self) -> &String;
 }
 
+// A provider that implements `Greeter`
 pub struct GreetHello;
 
 impl<Context> Greeter<Context> for GreetHello
+// The `Self` type is never used
 where
-    Context: HasName,
+    Context: HasName, // Inject the `name` dependency from `Context`
 {
     fn greet(context: &Context) {
         println!("Hello, {}!", context.name());
     }
 }
 
-#[derive(HasField)]
+// A concrete context that uses CGP components
+#[derive(HasField)] // Deriving `HasField` automatically implements `HasName`
 pub struct Person {
     pub name: String,
 }
 
+// The CGP components mapping for `Person`
 pub struct PersonComponents;
 
+// Set CGP components used by `Person` to be `PersonComponents`
 impl HasComponents for Person {
     type Components = PersonComponents;
 }
 
+// Compile-time wiring of CGP components
 delegate_components! {
     PersonComponents {
-        GreeterComponent: GreetHello,
+        GreeterComponent: GreetHello, // Use `GreetHello` to provide `Greeter`
     }
 }
 
@@ -46,5 +55,6 @@ fn main() {
         name: "Alice".into(),
     };
 
+    // `CanGreet` is automatically implemented
     person.greet();
 }
