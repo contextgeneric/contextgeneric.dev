848c0faa65af902e9849a7839e97ae5de0424130
Soares Chen <soares.chen@maybevoid.com>
2025-05-07 11:18:45 +0000

    Add section for check_components

--- code/bin/abstract_name.rs
+++ code/bin/abstract_name.rs
@@ -45,9 +45,17 @@ pub struct Person {
 delegate_components! {
     PersonComponents {
         NameTypeProviderComponent:
-            UseType<String>,
+            UseType<String>, // Instantiate the `Name` type to `String`
         GreeterComponent:
             GreetHello, // Use `GreetHello` to provide `Greeter`
+    }
+}
+
+// Compile-time checks that all dependencies are wired correctly
+check_components! {
+    CanUsePerson for Person {
+        NameTypeProviderComponent,
+        GreeterComponent,
     }
 }
 
