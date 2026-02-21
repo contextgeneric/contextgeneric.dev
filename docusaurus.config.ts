b873b1d7cc38bf2b5008a93e8b8140d34eaf24a7
Soares Chen <soares.chen@maybevoid.com>
2026-02-21 19:11:59 +0100

    Fix base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,11 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'http://localhost:3000',
+  url: 'https://contextgeneric.github.io',
   // url: 'https://contextgeneric.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cgp-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
